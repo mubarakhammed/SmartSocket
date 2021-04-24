c48bb0951efdee87a6e5ba8c7e19cb32509d6234
mubarak hammed <mubarakhammed00@gmail.com>
2021-04-24 05:32:46 -0700

    added some stuffs

--- app/src/main/java/com/example/smartsocket/ControlPanel.java
+++ app/src/main/java/com/example/smartsocket/ControlPanel.java
@@ -6,13 +6,12 @@ import androidx.core.content.ContextCompat;
 import android.os.Build;
 import android.os.Bundle;
 
-public class UserRegister extends AppCompatActivity {
+public class ControlPanel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_register);
-
+        setContentView(R.layout.activity_control_panel);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.black));
--- app/src/main/java/com/example/smartsocket/UserLogin.java
+++ app/src/main/java/com/example/smartsocket/UserLogin.java
@@ -1,15 +1,26 @@
 package com.example.smartsocket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.telephony.mbms.DownloadProgressListener;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class UserLogin extends AppCompatActivity {
 
     private TextView registerLink;
+    private Button Login;
+    private EditText email_input, password_input;
+    private  static final String TAG ="login";
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +28,31 @@ public class UserLogin extends AppCompatActivity {
         setContentView(R.layout.activity_user_login);
 
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.black));
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.black));
+        }
+
+        progressDialog= new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("logging in.... please wait");
+
+
         registerLink = (TextView) findViewById(R.id.registerLink);
+        Login = (Button) findViewById(R.id.loginUser) ;
+        email_input = (EditText) findViewById(R.id.user_email) ;
+        password_input = (EditText) findViewById(R.id.user_password) ;
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (email_input.getText().toString().isEmpty() || password_input.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Empty Fieldsnot allowed", Toast.LENGTH_LONG).show();
+                }
+                Intent register = new Intent(UserLogin.this, ControlPanel.class);
+                startActivity(register);
+            }
+        });
         registerLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +60,11 @@ public class UserLogin extends AppCompatActivity {
                 startActivity(register);
             }
         });
+    }
+
+
+
+    private  void logUser(){
+
     }
 }
